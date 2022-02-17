b176dce48a9285172536fc3f9b0bb1b13df7ae54
livacher <liva.chernova@gmail.com>
2022-02-17 08:12:58 +0200

    https://github.com/livacher/JS-Sessions/tree/main

---
+++ 15,02/script1.js
@@ -0,0 +1,53 @@
+let a = 1;
+let b = 2121.3232;
+// let bigInt = 22222222222222222n;
+let specialNumber = 5E-1;
+console.log(specialNumber);
+a = 2
+b = 1
+console.log(a+b);
+
+
+
+// 34323 = 3 * 10 ^0 + 2 * 10^1 + 3 * 10^3 + 4 * 10^4
+
+let str = "Here is the first line\nHere is another";
+
+console.log(str);
+
+let v1 = 5;
+let v2 = 10;
+console.log("Fifteen is ${v1 + v2}")
+
+a = b = 4;
+console.log(a);
+console.log(b);
+
+result = (a === b);
+console.log(result);
+
+a = 1;
+b = 2;
+
+console.log("First part " + a + b + " last part" );
+
+console.log("First part " + (a + b) + " last part" );
+
+let cond1 = true;
+let cond2 = false;
+let cond3 = true;
+let cond4 = false;
+
+result = !(cond1 == cond2 && cond3 == cond4);
+console.log(result);
+
+result = cond1 == cond2 && cond3 == cond4;
+console.log(result);
+
+a = 4;
+a++;
+console.log(a);
+console.log(a++);
+console.log(++a);
+console.log(a ** 2);
+
